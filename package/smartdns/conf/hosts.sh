116eaa60b2e18e39df2222f3759c57bd0629cb49
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-11-17 17:20:44 +0800

    Update hosts.sh

--- package/smartdns/conf/hosts.sh
+++ package/smartdns/conf/hosts.sh
@@ -2,4 +2,5 @@
 sed -i '/# GitHub520 Host Start/,/# Github520 Host End/d' /etc/hosts
 curl https://raw.hellogithub.com/hosts >> /etc/hosts
 /etc/init.d/dnsmasq restart
-/etc/init.d/AdGuardHome restart;/etc/init.d/mosdns restart
+/etc/init.d/AdGuardHome restart
+/etc/init.d/mosdns restart
