c0a14cdb644796c22b61b8c648496361d8d569c1
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-11-17 17:09:03 +0800

    Add files via upload

---
+++ package/smartdns/conf/hosts.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sed -i '/# GitHub520 Host Start/,/# Github520 Host End/d' /etc/hosts
+curl https://raw.hellogithub.com/hosts >> /etc/hosts
+/etc/init.d/dnsmasq restart
+/etc/init.d/AdGuardHome restart;/etc/init.d/mosdns restart
