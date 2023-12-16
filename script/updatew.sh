2d9aac124099816a4440bb65b4dd3b5621b7b941
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-12-16 17:54:11 +0800

    Update updatew.sh

--- script/updatew.sh
+++ script/updatew.sh
@@ -1,16 +1,18 @@
 # GFW List
-curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt  > /tmp/temp_direct1
-curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/apple-cn.txt   > /tmp/temp_direct2
-curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/google-cn.txt   > /tmp/temp_direct3
-sed "s/^full://g;s/^regexp:.*$//g;s/^/nameserver \//g;s/$/\/direct/g" -i /tmp/temp_direct
-cat /tmp/temp_direct > conf/domain-set/direct-domain-list.conf
+curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt > /tmp/temp_direct1
+curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/apple-cn.txt > /tmp/temp_direct2
+curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/google-cn.txt > /tmp/temp_direct3
+cat /tmp/temp_direct1 /tmp/temp_direct2 /tmp/temp_direct3 > /tmp/temp_direct
+ /tmp/temp_direct
+cat /tmp/temp_direct | sed "s/^full://g;s/^regexp:.*$//g;s/^/nameserver \//g;s/$/\/direct/g" -i conf/domain-set/direct-domain-list.conf
 
 # Update China List
-curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt  > /tmp/temp_proxy1
-curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/gfw.txt   > /tmp/temp_proxy2
+curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt > /tmp/temp_proxy1
+curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/gfw.txt > /tmp/temp_proxy2
 curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/greatfire.txt > /tmp/temp_proxy3
-sed "s/^full://g;s/^regexp:.*$//g;s/^/nameserver \//g;s/$/\/proxy/g" -i /tmp/temp_proxy
-cat /tmp/temp_proxy > conf/domain-set/proxy-domain-list.conf
+cat /tmp/temp_proxy1 /tmp/temp_proxy2 /tmp/temp_proxy3 script/cust_gfwdomain.conf | \
+    sort -u | sed 's/^\.*//g' > /tmp/temp_proxy
+cat /tmp/temp_proxy | sed "s/^full://g;s/^regexp:.*$//g;s/^/nameserver \//g;s/$/\/proxy/g" -i conf/domain-set/proxy-domain-list.conf
 
 # Update China IPV4 List
 qqwry="$(curl -kLfsm 5 https://raw.githubusercontent.com/metowolf/iplist/master/data/special/china.txt)"
