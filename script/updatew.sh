e11c32fbbe9cfdbc7bdaa00f9f6fb9c024224177
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-12-16 17:32:32 +0800

    Update updatew.sh

--- script/updatew.sh
+++ script/updatew.sh
@@ -1,34 +1,20 @@
-#!/bin/sh
-sed -i '/# GitHub520 Host Start/,/# Github520 Host End/d' /etc/hosts
-curl https://raw.hellogithub.com/hosts >> /etc/hosts
-/etc/init.d/dnsmasq restart
-/etc/init.d/AdGuardHome restart
-/etc/init.d/mosdns restart
+# GFW List
+curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt  > /tmp/temp_direct1
+curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/apple-cn.txt   > /tmp/temp_direct2
+curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/google-cn.txt   > /tmp/temp_direct3
+sed "s/^full://g;s/^regexp:.*$//g;s/^/nameserver \//g;s/$/\/direct/g" -i /tmp/temp_direct
+cat /tmp/temp_direct > conf/domain-set/direct-domain-list.conf
 
-rm /tmp/etc/smartdns/cn.conf    /tmp/etc/smartdns/domain-forwarding.list   /tmp/etc/smartdns/domain-block.list
+# Update China List
+curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt  > /tmp/temp_proxy1
+curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/gfw.txt   > /tmp/temp_proxy2
+curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/greatfire.txt > /tmp/temp_proxy3
+sed "s/^full://g;s/^regexp:.*$//g;s/^/nameserver \//g;s/$/\/proxy/g" -i /tmp/temp_proxy
+cat /tmp/temp_proxy > conf/domain-set/proxy-domain-list.conf
 
-curl  "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt"  >> /tmp/etc/smartdns/cn.conf
-curl  "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/apple-cn.txt"    >> /tmp/etc/smartdns/cn.conf
-curl  "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/google-cn.txt"   >> /tmp/etc/smartdns/cn.conf
-
-rm /etc/smartdns/cn.conf
-sed "s/^full://g;s/^regexp:.*$//g;s/^/nameserver \//g;s/$/\/cn/g" -i /tmp/etc/smartdns/cn.conf
-cat /tmp/etc/smartdns/cn.conf > /etc/smartdns/cn.conf
-
-curl  "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt"  >> /tmp/etc/smartdns/domain-forwarding.list
-curl  "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/gfw.txt"        >> /tmp/etc/smartdns/domain-forwarding.list
-curl  "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/greatfire.txt"  >> /tmp/etc/smartdns/domain-forwarding.list
-
-rm /etc/smartdns/domain-forwarding.list
-sed "s/^full://g;s/^regexp:.*$//g;s/^/nameserver \//g;s/$/\/gfwlist/g" -i /tmp/etc/smartdns/domain-forwarding.list
-cat /tmp/etc/smartdns/domain-forwarding.list > /etc/smartdns/domain-forwarding.list
-
-curl  "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/reject-list.txt"  >> /tmp/etc/smartdns/domain-block.list
-curl  "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/win-spy.txt"  >> /tmp/etc/smartdns/domain-block.list
-curl  "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/win-extra.txt"  >> /tmp/etc/smartdns/domain-block.list
-
-rm /etc/smartdns/domain-block.list
-sed "s/^full://g;s/^regexp:.*$//g;s/^/address \//g;s/$/\/#/g" -i /tmp/etc/smartdns/domain-block.list
-cat /tmp/etc/smartdns/domain-block.list > /etc/smartdns/domain-block.list
-
-/etc/init.d/smartdns restart
+# Update China IPV4 List
+qqwry="$(curl -kLfsm 5 https://raw.githubusercontent.com/metowolf/iplist/master/data/special/china.txt)"
+ipipnet="$(curl -kLfsm 5 https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt)"
+clang="$(curl -kLfsm 5 https://ispip.clang.cn/all_cn.txt)"
+iplist="$qqwry\n$ipipnet\n$clang"
+echo -e "${iplist}" | sort | uniq |sed -e '/^$/d' -e 's/^/blacklist-ip /g' > conf/blacklist-ip.conf
