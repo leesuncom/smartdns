86ab1fbef7fed643a1412bf0a57ab2374bfd5668
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-12-17 13:44:10 +0800

    Update updatew.sh

--- script/updatew.sh
+++ script/updatew.sh
@@ -16,6 +16,9 @@ cat /tmp/temp_gfwlist | sed -e '/^$/d' > /tmp/proxy-domain-list.conf
 sed "s/^full://g;s/^regexp:.*$//g;s/^/nameserver \//g;s/$/\/proxy/g" -i /tmp/proxy-domain-list.conf
 cat script/gfw_group.conf /tmp/proxy-domain-list.conf > conf/domain-set/proxy-domain-list.conf
 
+# Update address.conf
+curl -sS https://raw.githubusercontent.com/Cats-Team/AdRules/main/smart-dns.conf > conf/address.conf
+
 # Update China IPV4 List
 qqwry="$(curl -kLfsm 5 https://raw.githubusercontent.com/metowolf/iplist/master/data/special/china.txt)"
 ipipnet="$(curl -kLfsm 5 https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt)"
